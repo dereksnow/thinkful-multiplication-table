943f525b6be93d789e6f0393adcabb29eccb36ab
derek <derek.snow@gmail.com>
2014-07-27 16:12:04 -0500

    styling and use of scope events to broadcast/listen for events

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
 angular.module('myApp', [])
-    .controller('MultiplicationCtrl', function($scope, $attrs) {
+    .controller('DisplayCtrl', function($scope) {
+        $scope.$on('displayData', function(event, data) {
+            $scope.content = data;
+        });
+    })
+    .controller('MultiplicationCtrl', function($scope, $attrs, $rootScope) {
         function populateNumbers(x) {
             numbers = [];
             for (var i = 0; i < x; i++) {
@@ -13,5 +18,22 @@ angular.module('myApp', [])
         $scope.$watch('numberLimit', function(limit) {
             $scope.numbers = populateNumbers(limit);
         });
+
         $scope.numberLimit = $attrs.initialNumberLimit || 10;
+
+        var activeFactorA, activeFactorB;
+        $scope.setActiveFactors = function(a, b) {
+            activeFactorA = a;
+            activeFactorB = b;
+        };
+
+        $scope.matchesFactor = function(a, b) {
+            return a === activeFactorA || b === activeFactorB;
+        };
+        $scope.clearActiveFactors = function() {
+            activeFactorA = activeFactorB = null;
+        };
+	$scope.setActiveNumber = function(number) {
+		$rootScope.$broadcast('displayData', number);
+	};
     });
